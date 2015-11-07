729d623bbc22e617f6a917fb8abb0bfa6bbadf0b
Monique Shotande <momoshog@ufl.edu>
2015-11-07 01:33:59 -0500

    Event Handling for user login

--- js/users.js
+++ js/users.js
@@ -1,7 +1,18 @@
-var users = {
-	[username: "MoSho",
-		password: "helloworld"],
-	[username: "JoYoung",
-		password: "hiworld"]
+var users = [
+	
+		{"username": "MoSho",
+			"password": "helloworld"
+		},
+		{"username": "JoYoung",
+			"password": "hiworld"
+		}
 
-};
+];
+
+var loginbtn = document.getElementById("Login");
+loginbtn.addEventListener("click", function() {
+	var usrbtn = document.getElementById("Username");
+	var passbtn = document.getElementById("password");
+
+	alert("CLICK!");
+});
