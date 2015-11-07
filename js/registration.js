b8e95930c7c9a7cda76c38c3474deb37f4f514ec
jonepl <jonepl.ufl@gmail.com>
2015-11-07 06:44:38 -0500

    New stuff

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,8 @@
 /**
  * Created by Purnell on 11/7/2015.
  */
+
+var regAnnual = document.getElementById("Annual");
+var regRLC = document.getElementById("RLC");
+var regFRC = document.getElementById("FRC");
+
